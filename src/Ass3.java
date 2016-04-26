e26b14ac61b766006dfd1b6985f656e91276dc35
Yeehong <Yeehong@10.248.102.223>
2016-04-26 11:43:41 +1000

    Edited the comment

--- src/Ass3.java
+++ src/Ass3.java
@@ -3,7 +3,7 @@ public class Ass3 {
 
 	public static void main(String[] args) {
 		
-		//yeehong: Always Pull first before you want to start your work..
+		//yeehong: Always Pull first before you want to start your work & Commit.
 		//Andy Zhang
 		//Winnie Lu - winlu89
 
