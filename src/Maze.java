86d0921bd032fb3f4933cb0c86b889ea97f3ea59
Yeehong <Yeehong@Yeehong>
2016-05-05 21:19:57 +1000

    Edited a word

--- src/Maze.java
+++ src/Maze.java
@@ -4,7 +4,7 @@ public class Ass3{
 	public static void main(String[] args)
 	{
 		Yeehong:
-		Apparently no errors will be appear if you start coding and
+		Apparently no errors will appear if you start coding and
 		I not sure why. So I guess we could just create an external project
 		in our Eclipse and just copy paste to and forth along the way for now.
 	}
