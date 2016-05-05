445bc53df0dbba73df20e6567267aeec3ae0547d
Yeehong <Yeehong@Yeehong>
2016-05-06 01:47:42 +1000

    Edited comment/class name

--- src/Maze.java
+++ src/Maze.java
@@ -3,7 +3,7 @@ public class Maze{
 
 	public static void main(String[] args)
 	{		
-		Yeehong:
+		Yeehong
 		Apparently no errors will appear if you start coding and
 		I'm not sure why. So I guess for now we could just create 
 		an external project in our Eclipse and if we're done coding,
