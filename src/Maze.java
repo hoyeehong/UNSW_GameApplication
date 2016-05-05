d7102df096187b335e4a00bbdaf9cff7f888cc98
Yeehong <Yeehong@Yeehong>
2016-05-05 21:14:59 +1000

    Renamed class file and added important comments.

---
+++ src/Maze.java
@@ -0,0 +1,12 @@
+
+public class Ass3{
+
+	public static void main(String[] args)
+	{
+		Yeehong:
+		Apparently no errors will be appear if you start coding and
+		I not sure why. So I guess we could just create an external project
+		in our Eclipse and just copy paste to and forth along the way for now.
+	}
+
+}
